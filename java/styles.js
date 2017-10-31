2555bef20171c1aa794bb1a24954da646478601d
Bwana <shirohigemw@gmail.com>
2017-10-31 09:50:43 +0300

    fix meta tag utf-8

--- java/styles.js
+++ java/styles.js
@@ -1,29 +1,30 @@
+$("#userInput form").submit(function(event) {
 
-    $("#userInput form").submit(function(event) {
+var arr = [];
 
-        var arr = [];
-        var num = parseInt($("input#number").val());
+var num = parseInt($("input#number").val());
 
-        for (var i = 1; i <= num; i++) {
-            arr.push([i]);
-        }
+for (var i = 1; i <= num; i++) {
+  arr.push([i]);
+}
 
-        for (var j = 0; j < arr.length; j++) {
+for (var j = 0; j < arr.length; j++) {
 
-            if (arr[j] % 3 === 0 && arr[j] % 5 === 0) {
-                $(".result").append("<li>PingPong</li>");
-            } else if (arr[j] % 5 === 0) {
-                $(".result").append("<li>Pong</li>");
-            } else if (arr[j] % 3 === 0) {
-                $(".result").append("<li>Ping</li>");
-            } else {
-                $(".result").append("<li>" + arr[j] + "</li>");
-            }
-        }
+  if (arr[j] % 3 === 0 && arr[j] % 5 === 0) {
+    $(".result").append("<li>PingPong</li>");
+  } else if (arr[j] % 5 === 0) {
+    $(".result").append("<li>Pong</li>");
+  } else if (arr[j] % 3 === 0) {
+    $(".result").append("<li>Ping</li>");
+  } else {
+    $(".result").append("<li>" + arr[j] + "</li>");
+  }
+}
 
-        $(".foot").show();
-        // $("#output").show();
 
-        event.preventDefault();
-    });
+$(".result").show();
+
+// $("#output").show();
+
+event.preventDefault();
 });
